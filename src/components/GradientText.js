761c8c9e0f4054fad0a0ab29c1e9aba509a43fce
Yug Mehta <yug26mehta@gmail.com>
2026-04-05 16:15:26 +0530

    change UTF

--- src/components/GradientText.js
+++ src/components/GradientText.js
@@ -1,4 +1,4 @@
-﻿import { useState, useCallback, useEffect, useRef } from "react";
+import { useState, useCallback, useEffect, useRef } from "react";
 import { motion, useMotionValue, useAnimationFrame, useTransform } from "framer-motion";
 import "./GradientText.css";
 
